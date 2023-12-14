53cdbf798e82c2229017296b84e93cd2fe98c6f0
ipds3501 <121912558+ipds3501@users.noreply.github.com>
2023-12-14 16:16:53 +0700

    Update administrasi.js

--- administrasi.js
+++ administrasi.js
@@ -86,7 +86,7 @@ administrasi_json = [
 			{
 				"nama": "SiMistik",
 				"desc": "Sistem Informasi Mitra Statistik",
-				"ikon": "https://i.imgur.com/LKjmjn4.png",
+				"ikon": "https://i.imgur.com/g7Rb3d1.png",
 			  	"url": "https://sites.google.com/view/simistik/beranda"
 			}
 			
