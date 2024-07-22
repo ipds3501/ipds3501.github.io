8a2dccce4bfd7507d4c7ee43c6018014477c0cfb
ipds3501 <121912558+ipds3501@users.noreply.github.com>
2024-07-22 10:23:06 +0700

    Update kehumasan.js

--- kehumasan.js
+++ kehumasan.js
@@ -32,18 +32,9 @@ kehumasan_json = [
 			{
 			  	nama: "Beban Kerja Humas",
 			  	desc: "Jadwal Pembagian Tim Humas BPS Kabupaten Pacitan",
-			  	ikon: "https://drive.google.com/file/d/17FP390pYopj9nbB8_6-9vaBy-nuSz3QP/view?usp=sharing",
+			  	ikon: "https://i.imgur.com/LH3AJez.png",
 			  	url: "https://taplink.cc/jadwaltugashumas_bps3501"
 			}
 
-			//salin di bawah baris spasi ini 
-
-			];
-
-// Untuk menambahkan tautan, salin kode berikut, hapus 2 garis miring dan tempelkan dibaris paling bawah dari kode diatas
-			// ,{
-			// 	nama: "nama aplikasi",
-			// 	desc: "deskripsi aplikasi",
-			// 	ikon: "ikon aplikasi",
-			//  url: "tautan aplikasi"
-			// }
+			
+			]
