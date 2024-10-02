5b5c41d639a34ec9104b1d8f398ece7a36550aa6
ipds3501 <121912558+ipds3501@users.noreply.github.com>
2024-10-02 10:37:25 +0700

    Update pelayanan.js

--- pelayanan.js
+++ pelayanan.js
@@ -30,6 +30,12 @@ pelayanan_json = [
 				url: "https://forms.gle/HXFTusi9xBC3f2DT7"
 			},
 			{
+				nama:  "ROMANTIK".
+				desc:  "Layanan Bagi Instansi Pemerintah untuk Mendapatkan Rekomendasi Penyelenggaraan Kegiatan Statistik Sektoral dari BPS.",
+				ikon:  "https://imgur.com/mRP0Tsc.jpg",
+				url:  "https://romantik.web.bps.go.id/"
+			},	
+			{
 				nama: "Survey Kepuasan Layanan PST",
 				desc: "Sahabatdata, bantu kesediaannya untuk mengisi Survey Kepuasan Layanan kami",
 				ikon: "https://i.imgur.com/Y9XgGOO.jpeg",
