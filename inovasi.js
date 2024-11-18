815d887102bd11cbba67092ddae5c6de11fd318d
ipds3501 <121912558+ipds3501@users.noreply.github.com>
2024-11-18 23:14:10 +0700

    Update inovasi.js

--- inovasi.js
+++ inovasi.js
@@ -3,21 +3,38 @@ inovasi_json = [
 			  	nama: "DPace",
 			  	desc: "Portal data-data Pacitan dalam genggaman.",
 			  	ikon: "https://i.imgur.com/RAxl26w.png",
-			  	url: "https://dpace.herokuapp.com"
+			  	url: "https://dpace.herokuapp.com/"
 			},
 			{
-			  	nama: "Gelatik",
-			  	desc: "Geliat Cinta Statistik, Webinar untuk mengenal statistik lebih asik.",
-			  	ikon: "https://i.imgur.com/0Su7Qio.png",
-			  	url: "https://www.youtube.com/playlist?list=PLFUTKHkQj04ovTMFqkw6vABUe_E3Q0Wcj"
+			  	nama: "RAMUAN IMPIAN DAKU",
+			  	desc: "Rapat Mingguan dengan Pimpinan Bertindak Lanjut.",
+			  	ikon: "https://i.imgur.com/6IebV1U.png",
+			  	url: "https://drive.google.com/drive/folders/1Cy9LdEe60lJL-k2JsYhvHLpeaflbm6ly?usp=sharing"
 			},
 			{
-				nama:  "ROMANTIK",
-				desc:  "Layanan Bagi Instansi Pemerintah untuk Mendapatkan Rekomendasi Penyelenggaraan Kegiatan Statistik Sektoral dari BPS.",
-				ikon:  "https://imgur.com/mRP0Tsc.jpg",
-				url:  "https://romantik.web.bps.go.id/"
+			  	nama: "SSWW",
+			  	desc: "Sat Set Wat Wet",
+			  	ikon: "https://i.imgur.com/KnxKVN5.png",
+			  	url: "https://drive.google.com/drive/folders/1kg3FQSmJ6PvoHf82_gIzt20aHnwEQLad?usp=sharing"
 			},
-		
+			{
+			  	nama: "A.Gi.To",
+			  	desc: "Apel Pagi Tetap Oke",
+			  	ikon: "https://i.imgur.com/9IVpyzW.png",
+			  	url: "https://drive.google.com/drive/folders/1--ycWbP3d6xSsXYjStKoFNfuv-RYAv4x?usp=sharing"
+			},
+			{
+			  	nama: "GASAK",
+			  	desc: "Ganti, piSAh, Konsolidasi untuk lingkungan yang kondusif",
+			  	ikon: "https://i.imgur.com/yYlzdEr.png",
+			  	url: "https://drive.google.com/drive/folders/1JN3cxPQ8Dconq3qI1brH6cKEbl2_q0Oq?usp=sharing"
+			},
+			{
+				nama: "LaporKang",
+				desc: "Layanan untuk menampung aspirasi dan keluhan dari masyarakat secara online.",
+				ikon: "https://i.imgur.com/acG7P0U.jpg",
+				url: "https://forms.gle/HXFTusi9xBC3f2DT7"
+			},
 	
 			//salin di bawah baris spasi ini 
 
