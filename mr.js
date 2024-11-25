53c50a4d70b480611a86d471019db9d2613ade54
ipds3501 <121912558+ipds3501@users.noreply.github.com>
2024-11-25 08:52:01 +0700

    Create mr.js

---
+++ mr.js
@@ -0,0 +1,24 @@
+mr_json = [
+			                {
+			            	        nama: "DPace",
+			  	                  desc: "Portal data-data Pacitan dalam genggaman.",
+			  	                  ikon: "https://i.imgur.com/RAxl26w.png",
+			  	                  url: "https://dpace.herokuapp.com/"
+			                },
+			                {
+			  	                  nama: "RAMUAN IMPIAN DAKU",
+			  	                  desc: "Rapat Mingguan dengan Pimpinan Bertindak Lanjut.",
+			  	                  ikon: "https://i.imgur.com/6IebV1U.png",
+			  	                  url: "https://drive.google.com/drive/folders/1Cy9LdEe60lJL-k2JsYhvHLpeaflbm6ly?usp=sharing"
+			                },
+  			              //salin di bawah baris spasi ini 
+
+			                ];
+
+// Untuk menambahkan tautan, salin kode berikut, hapus 2 garis miring dan tempelkan dibaris paling bawah dari kode diatas
+			// ,{
+			// 	nama: "nama aplikasi",
+			// 	desc: "deskripsi aplikasi",
+			// 	ikon: "ikon aplikasi",
+			//  url: "tautan aplikasi"
+			// }
