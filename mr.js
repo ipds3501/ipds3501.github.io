85a5c0c226722f1a56396199040aba02ffd7fda0
ipds3501 <121912558+ipds3501@users.noreply.github.com>
2025-02-03 12:26:22 +0700

    Update mr.js

--- mr.js
+++ mr.js
@@ -1,73 +1,35 @@
-mr_json = [ 
-			                {
-			            	        nama: "Kepala BPS Kabupaten Pacitan",
-			  	                desc: "Manajemen Resiko Kepala BPS Kabupaten Pacitan.",
-			  	                ikon: "https://i.imgur.com/PLYhp4h.png",
-			  	                url: "https://drive.google.com/drive/folders/1X96nchDLA9qJkNbnmK0oqelM7Ko4Oy6I?usp=sharing"
-			                },
-			                {
-			  	                nama: "Sub Bagian Umum dan SAKIP",
-			  	                desc: "Manajamen Resiko Kepala Sub Bagian Umum dan SAKIP.",
-			  	                ikon: "https://i.imgur.com/3JYLvSh.png",
-			  	                url: "https://drive.google.com/drive/folders/171cbmiRS1Vp77icn5pFgred7o7kOJU4B?usp=sharing"
-			                },
-					{
-			  	                nama: "Tim Statistik Sosial dan Desa Cantik",
-			  	                desc: "Manajamen Resiko Tim Statistik Sosial dan Desa Cinta Statistik.",
-			  	                ikon: "https://i.imgur.com/pQcPTVZ.png",
-			  	                url: "https://drive.google.com/drive/folders/1tJZAWGtKKyaCg6RuGItOgdDTpDmcFxvm?usp=sharing"
-			                },
-				 	{
-			  	                nama: "Tim Statistik Pertanian",
-			  	                desc: "Manajamen Resiko Tim Statistik Pertanian.",
-			  	                ikon: "https://i.imgur.com/JNzawxA.png",
-			  	                url: "https://drive.google.com/drive/folders/1eKUeLwfqp4WJ7AsPe9SYWO0VkoLRXQa5?usp=sharing"
-			                },
-	 				{
-			  	                nama: "Tim Statistik Distribusi",
-			  	                desc: "Manajamen Resiko Tim Statistik Distribusi.",
-			  	                ikon: "https://i.imgur.com/1YDC6H2.png",
-			  	                url: "https://drive.google.com/drive/folders/1XjVvLQGFAwbHeVp5vF_abKqjb4BPRwKq?usp=sharing"
-			                },
-					{
-			  	                nama: "Tim Neraca Regional dan Analisis & SDI - IPS",
-			  	                desc: "Manajamen Resiko Tim Neraca Regional dan Analisis serta Satu Data Indonesia dan Indeks Penilaian Statistik.",
-			  	                ikon: "https://i.imgur.com/X8Axr7e.png",
-			  	                url: "https://drive.google.com/drive/folders/1SjsGDlcx73GF643Ucl2TexwXAoR4whC4?usp=sharing"
-			                },
-					{
-			  	                nama: "Tim IPDS",
-			  	                desc: "Manajamen Resiko Tim IPDS.",
-			  	                ikon: "https://i.imgur.com/O6PMOys.png",
-			  	                url: "https://drive.google.com/drive/folders/1Zs2-KyuMiVAptPLiYdGtgiTIHjONwssZ?usp=sharing"
-			                },
-					{
-			  	                nama: "Tim Industri dan PEK",
-			  	                desc: "Manajamen Resiko Tim Industri dan Pertambangan, Energi dan Konstruksi.",
-			  	                ikon: "https://i.imgur.com/TKDVbp2.png",
-			  	                url: "https://drive.google.com/drive/folders/1axfERrjc6wEYyr5yDOs_MnnnT6OKK9d8?usp=sharing"
-			                },
-					{
-			  	                nama: "Tim Pojok Statistik dan PEKPPP",
-			  	                desc: "Manajamen Resiko Tim Pojok Statistik dan PEKPPP.",
-			  	                ikon: "https://i.imgur.com/PnqPjUo.png",
-			  	                url: "https://drive.google.com/drive/folders/1Un7GsxfueYCA5_28QCREEijC7DAAhv0t?usp=sharing"
-			                },
-					{
-			  	                nama: "Tim HUMAS",
-			  	                desc: "Manajamen Resiko Tim Hubungan Masyarakat.",
-			  	                ikon: "https://i.imgur.com/yoELOpw.jpeg",
-			  	                url: "https://drive.google.com/drive/folders/1FZQyQXIft7vkMwC7HrQaUixNZkgxoCh3?usp=sharing"
-			                },
-	
-  			              //salin di bawah baris spasi ini 
+const mr_json = [ 
+    {
+        nama: "Kepala BPS Kabupaten Pacitan",
+        desc: "Manajemen Resiko Kepala BPS Kabupaten Pacitan.",
+        ikon: "https://i.imgur.com/PLYhp4h.png",
+        url: "https://drive.google.com/drive/folders/1X96nchDLA9qJkNbnmK0oqelM7Ko4Oy6I?usp=sharing",
+        tahun: 2024
+    },
+    {
+        nama: "Sub Bagian Umum dan SAKIP",
+        desc: "Manajamen Resiko Kepala Sub Bagian Umum dan SAKIP.",
+        ikon: "https://i.imgur.com/3JYLvSh.png",
+        url: "https://drive.google.com/drive/folders/171cbmiRS1Vp77icn5pFgred7o7kOJU4B?usp=sharing",
+        tahun: 2025
+    }
+];
 
-			                ];
+function generateMenu(tahun) {
+    return mr_json.filter(item => item.tahun === tahun).map(item => `
+        <div class="col">
+            <a href="${item.url}" target="_blank">
+                <div class="card shadow-sm text-center">
+                    <div class="card-body p-2">
+                        <img src="${item.ikon}" width="50">
+                        <h5>${item.nama}</h5>
+                        <p>${item.desc}</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    `).join('');
+}
 
-// Untuk menambahkan tautan, salin kode berikut, hapus 2 garis miring dan tempelkan dibaris paling bawah dari kode diatas
-			// ,{
-			// 	nama: "nama aplikasi",
-			// 	desc: "deskripsi aplikasi",
-			// 	ikon: "ikon aplikasi",
-			//  url: "tautan aplikasi"
-			// }"
+document.getElementById("menu2024").innerHTML = generateMenu(2024);
+document.getElementById("menu2025").innerHTML = generateMenu(2025);
