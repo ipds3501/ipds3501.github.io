db75818d15f429f171976f2ac1df27da1bbfa925
ipds3501 <121912558+ipds3501@users.noreply.github.com>
2025-02-03 12:30:29 +0700

    Update mr.js

--- mr.js
+++ mr.js
@@ -1,35 +1,49 @@
-const mr_json = [ 
-    {
-        nama: "Kepala BPS Kabupaten Pacitan",
-        desc: "Manajemen Resiko Kepala BPS Kabupaten Pacitan.",
-        ikon: "https://i.imgur.com/PLYhp4h.png",
-        url: "https://drive.google.com/drive/folders/1X96nchDLA9qJkNbnmK0oqelM7Ko4Oy6I?usp=sharing",
-        tahun: 2024
-    },
-    {
-        nama: "Sub Bagian Umum dan SAKIP",
-        desc: "Manajamen Resiko Kepala Sub Bagian Umum dan SAKIP.",
-        ikon: "https://i.imgur.com/3JYLvSh.png",
-        url: "https://drive.google.com/drive/folders/171cbmiRS1Vp77icn5pFgred7o7kOJU4B?usp=sharing",
-        tahun: 2025
-    }
-];
+$("#mr_2024_box").click(function(){
+    $("#exampleModalLabel").text("Manajemen Resiko 2024");
+    $("#submenu_container").html('');
+    $("#add_btn").attr("href", "https://github.com/ipds3501/ipds3501.github.io/blob/master/mr.js");
 
-function generateMenu(tahun) {
-    return mr_json.filter(item => item.tahun === tahun).map(item => `
-        <div class="col">
-            <a href="${item.url}" target="_blank">
-                <div class="card shadow-sm text-center">
-                    <div class="card-body p-2">
-                        <img src="${item.ikon}" width="50">
-                        <h5>${item.nama}</h5>
-                        <p>${item.desc}</p>
+    for (var i = 0; i <= mr_dict.length - 1; i++) {
+        if (mr_dict[i].tahun === 2024) {
+            $("#submenu_container").append(`
+                <a class="col p-0 m-0 text-dark" target="_blank" href="${mr_dict[i].url}">
+                    <div class="box p-1">
+                        <div class="our-services border border-secondary">
+                            <div class="iconm bg-light w-75 d-flex align-items-center justify-content-center">
+                                <img id="icon_apl" src="${mr_dict[i].ikon}" class="oversize p-1">
+                            </div>
+                            <p class="fw-light mt-2" id="desc_apl">
+                                <strong class="h6">${mr_dict[i].nama}</strong><br>${mr_dict[i].desc}
+                            </p>
+                        </div>
                     </div>
-                </div>
-            </a>
-        </div>
-    `).join('');
-}
+                </a>
+            `);
+        }
+    }
+});
 
-document.getElementById("menu2024").innerHTML = generateMenu(2024);
-document.getElementById("menu2025").innerHTML = generateMenu(2025);
+$("#mr_2025_box").click(function(){
+    $("#exampleModalLabel").text("Manajemen Resiko 2025");
+    $("#submenu_container").html('');
+    $("#add_btn").attr("href", "https://github.com/ipds3501/ipds3501.github.io/blob/master/mr.js");
+
+    for (var i = 0; i <= mr_dict.length - 1; i++) {
+        if (mr_dict[i].tahun === 2025) {
+            $("#submenu_container").append(`
+                <a class="col p-0 m-0 text-dark" target="_blank" href="${mr_dict[i].url}">
+                    <div class="box p-1">
+                        <div class="our-services border border-secondary">
+                            <div class="iconm bg-light w-75 d-flex align-items-center justify-content-center">
+                                <img id="icon_apl" src="${mr_dict[i].ikon}" class="oversize p-1">
+                            </div>
+                            <p class="fw-light mt-2" id="desc_apl">
+                                <strong class="h6">${mr_dict[i].nama}</strong><br>${mr_dict[i].desc}
+                            </p>
+                        </div>
+                    </div>
+                </a>
+            `);
+        }
+    }
+});
