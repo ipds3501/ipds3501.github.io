f731305b6584cdf610e188f651438d2f39a837d6
ipds3501 <121912558+ipds3501@users.noreply.github.com>
2025-02-03 17:12:49 +0700

    Update mr.js

--- mr.js
+++ mr.js
@@ -49,22 +49,71 @@ const mr_json = [
     {
         nama: "Kepala BPS Kabupaten Pacitan",
         year: 2024,
-        url: "https://example.com/2024/1",
-        ikon: "https://via.placeholder.com/50",
+        url: "https://drive.google.com/drive/folders/1ocYrH_BkKabTLtkxJrhGvhC2RS5QOQQ1?usp=drive_link",
+        ikon: "https://i.imgur.com/PLYhp4h.png",
+        desc: "Manajemen Resiko Kepala BPS Kabupaten Pacitan."
+    },
+        {
+        nama: "Kepala BPS Kabupaten Pacitan",
+        year: 2025,
+        url: "https://drive.google.com/drive/folders/1rU1jpLN9h0C0l5RHhDRJI5ftoCPzc8hg?usp=sharing",
+        ikon: "https://i.imgur.com/PLYhp4h.png",
         desc: "Manajemen Resiko Kepala BPS Kabupaten Pacitan."
     },
     {
         nama: "Tim Statistik Sosial dan Desa Cantik",
         year: 2024,
         url: "https://example.com/2024/2",
-        ikon: "https://via.placeholder.com/50",
+        ikon: "https://i.imgur.com/pQcPTVZ.png",
         desc: "Manajemen Resiko Tim Statistik Sosial dan Desa Cantik."
     },
     {
         nama: "Tim Statistik Pertanian",
-        year: 2025,
-        url: "https://example.com/2025/1",
-        ikon: "https://via.placeholder.com/50",
+        year: 2024,
+        url: "https://drive.google.com/drive/folders/1eKUeLwfqp4WJ7AsPe9SYWO0VkoLRXQa5?usp=sharing",
+        ikon: "https://i.imgur.com/JNzawxA.png",
         desc: "Manajemen Resiko Tim Statistik Pertanian."
+    },
+    {
+        nama: "Tim Statistik Distribusi",
+        year: 2024,
+        url: "https://drive.google.com/drive/folders/1XjVvLQGFAwbHeVp5vF_abKqjb4BPRwKq?usp=sharing",
+        ikon: "https://i.imgur.com/1YDC6H2.png",
+        desc: "Manajemen Resiko Tim Statistik Distribusi."
+    },
+    {
+        nama: "Tim Neraca Regional dan Analisis & SDI - IPS",
+        year: 2024,
+        url: "https://drive.google.com/drive/folders/1SjsGDlcx73GF643Ucl2TexwXAoR4whC4?usp=sharing",
+        ikon: "https://i.imgur.com/X8Axr7e.png",
+        desc: "Manajemen Resiko Tim Neraca Regional dan Analisis & SDI - IPS."
+    },
+    {
+        nama: "Tim IPDS",
+        year: 2024,
+        url: "https://drive.google.com/drive/folders/1Zs2-KyuMiVAptPLiYdGtgiTIHjONwssZ?usp=sharing",
+        ikon: "https://i.imgur.com/O6PMOys.png",
+        desc: "Manajamen Resiko Tim IPDS."
+    },
+    {
+        nama: "Tim Industri dan PEK",
+        year: 2024,
+        url: "https://drive.google.com/drive/folders/1axfERrjc6wEYyr5yDOs_MnnnT6OKK9d8?usp=sharing",
+        ikon: "https://i.imgur.com/TKDVbp2.png",
+        desc: "Manajamen Resiko Tim Industri dan PEK."
+    },   
+    {
+        nama: "Tim Pojok Statistik dan PEKPPP",
+        year: 2024,
+        url: "https://drive.google.com/drive/folders/1Un7GsxfueYCA5_28QCREEijC7DAAhv0t?usp=sharing",
+        ikon: "https://i.imgur.com/PnqPjUo.png",
+        desc: "Manajamen Resiko Tim Pojok Statistik dan PEKPPP."
+    }, 
+    {
+        nama: "Tim HUMAS",
+        year: 2024,
+        url: "https://drive.google.com/drive/folders/1FZQyQXIft7vkMwC7HrQaUixNZkgxoCh3?usp=sharing",
+        ikon: "https://i.imgur.com/yoELOpw.jpeg",
+        desc: "Manajamen Resiko Tim Hubungan Masyarakat."
     }
 ];
